09cf9cb91ad3802bbde155967ff576171a317789
jojododoe1212 <jojododoe1212@gmail.com>
2020-06-17 01:11:29 -0500

    first deployment

---
+++ scrape.py
@@ -0,0 +1,34 @@
+import requests
+import re
+
+from twilio.rest import Client
+from bs4 import BeautifulSoup
+
+# Your Account SID from twilio.com/console
+account_sid = "AC8672ae56238820de18b0bdc80dfee2ee"
+# Your Auth Token from twilio.com/console
+auth_token  = "cb983873c2bd4854f2b12a737f9f005c"
+client = Client(account_sid, auth_token)
+
+response = requests.get('https://www.gov.mb.ca/health/')
+soup = BeautifulSoup(response.text, 'html.parser');
+news = soup.findAll('p')
+
+link_news = news[0]
+covid_cases = link_news.contents[0].text
+date = link_news.em.text
+if("COVID-19 Bulletin" in covid_cases):
+	link = requests.get(link_news.a['href'])
+	soup_link = BeautifulSoup(link.text, 'html.parser')
+	for paragraph in soup_link.select('.content-section'):
+		announcement = paragraph.div.text
+		match = re.search(r'(\S+) new', announcement)
+		if match:
+			cases = match.group(1)
+sms_body = date + ", Manitoba has " + cases + " new case(s)."
+
+message = client.messages.create(
+    to="+12049156184", 
+    from_="+12058989300",
+    body="Hello, on " + sms_body)
+
