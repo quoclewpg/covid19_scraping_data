b33e705da05cfc1ff8a904a88bd397cab6f87994
jojododoe1212 <jojododoe1212@gmail.com>
2020-06-17 14:17:37 -0500

    edit loops

--- scrape.py
+++ scrape.py
@@ -14,21 +14,21 @@ response = requests.get('https://www.gov.mb.ca/health/')
 soup = BeautifulSoup(response.text, 'html.parser');
 news = soup.findAll('p')
 
-link_news = news[0]
-covid_cases = link_news.contents[0].text
-date = link_news.em.text
-if("COVID-19 Bulletin" in covid_cases):
-	link = requests.get(link_news.a['href'])
-	soup_link = BeautifulSoup(link.text, 'html.parser')
-	for paragraph in soup_link.select('.content-section'):
-		announcement = paragraph.div.text
-		match = re.search(r'(\S+) new', announcement)
-		if match:
-			cases = match.group(1)
+for i in reversed(range(len(news)-2)):
+	link_news = news[i]
+	covid_cases = link_news.contents[0].text
+	date = link_news.em.text
+	if("COVID-19 Bulletin" in covid_cases):
+		link = requests.get(link_news.a['href'])
+		soup_link = BeautifulSoup(link.text, 'html.parser')
+		for paragraph in soup_link.select('.content-section'):
+			announcement = paragraph.div.text
+			match = re.search(r'(\S+) new', announcement)
+			if match:
+				cases = match.group(1)
 sms_body = date + ", Manitoba has " + cases + " new case(s)."
 
 message = client.messages.create(
     to="+12049156184", 
     from_="+12058989300",
     body="Hello, on " + sms_body)
-
